8b4c1aee6e1d674d98969561a60329c04285ec69
Cristian Perez <cperez@negoziacf.com>
2022-10-12 22:46:32 -0500

    feature: mobile menu added

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const email = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const mobileMenuIcon = document.querySelector('.mobile-menu-icon')
+const mobileMenu = document.querySelector('.mobile-menu')
 
-email.addEventListener('click', triggerShowMenu)
+email.addEventListener('click', () => {triggerMenu("desktop")})
+mobileMenuIcon.addEventListener('click', () => {triggerMenu("mobile")})
 
-function triggerShowMenu() {
-    desktopMenu.classList.toggle('inactive')
+function triggerMenu(menuType) {
+    if(menuType === "desktop") desktopMenu.classList.toggle('inactive')
+    if(menuType === "mobile") mobileMenu.classList.toggle('inactive')
 }
